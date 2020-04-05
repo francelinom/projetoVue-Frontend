c6d9165624a2c29a7779353bc7ecbf6638f66ddd
Francelino Marcilio <fmarcilio1989@gmail.com>
2020-04-05 20:57:44 -0300

    cadastro de usuarios

--- src/components/admin/UserAdmin.vue
+++ src/components/admin/UserAdmin.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="user-admin">
-      <h1>Usuário componente</h1>
+      <b-table  hover striped :items="users" :fields="fields"></b-table>
   </div>
 </template>
 
 <script>
 
-import api from '@/api'
+import { baseApiUrl } from '../../global'
+import axios from 'axios'
 
 export default {
     name: 'UserAdmin',
@@ -14,14 +15,22 @@ export default {
       return{
         mode: 'save',
         user: {},
-        users: []
+        users: [],
+        fields: [
+          { key: 'id', label: 'Código', sortable: true},
+          { key: 'name', label: 'Nome', sortable: true},
+          { key: 'email', label: 'E-mail', sortable: true},
+          { key: 'admin', label: 'Administrador', sortable: true,
+              formatter: value => value ? 'Sim' : 'Não'},
+          {key: 'actions', label: 'Ações'}
+        ]
       }
     },
     methods:{
       loadUsers(){
-        api.get('/clientes').then(res =>{
+        const url = `${baseApiUrl}/users`
+        axios.get(url).then(res =>{
           this.users = res.data
-          console.log(this.users)
         })
       }
     },
