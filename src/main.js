dec33388c518beab4e24ed5e8f54a729b8f527ab
Francelino Marcilio <fmarcilio1989@gmail.com>
2020-04-10 15:01:48 -0300

    ajustando store

--- src/main.js
+++ src/main.js
@@ -10,8 +10,7 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-//TEMPORARIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkZyYW5jZWxpbm8gTWFyY2lsaW8iLCJlbWFpbCI6ImZyYW5jZWxpbm9AZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU4NjUxMTE5NiwiZXhwIjoxNTg2NzcwMzk2fQ.kS2G62LIaoId5EPTESf01yqTwnE0KZjRgSAjbwDuAxU'
+
 
 new Vue({
   store,
